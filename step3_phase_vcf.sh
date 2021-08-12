0ca43dc5f98963cc2a972ec60ece397f76cfdf63
Parker Wilson <parkerw@wustl.edu>
2021-08-12 11:39:10 -0500

    Update step3_phase_vcf.sh

--- step3_phase_vcf.sh
+++ step3_phase_vcf.sh
@@ -107,6 +107,11 @@ elif [ $verbose = "false" ]; then
   outputlog=$workdir/log.out
 fi
 
+# remove filtered variants
+echo "Removing filtered variants from input vcf"
+bcftools view -Oz -f PASS $inputvcf > /tmp/filter.vcf.gz
+bcftools index --tbi --threads $threads /tmp/filter.vcf.gz 
+
 # workflow for phasing vcf genotypes
 mkdir -p $outputdir
 
